de0517198cc9319f1ae7ed1919384ade987d8416
brettjayp <brettjayp@gmail.com>
2019-08-13 20:42:37 -0700

    adding questions in js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,24 @@
 'use strict';
 
 // List of variables I plan to create:
-// likesTravel, likesDogs, scubaDive, hasPets, rickMorty, procrastinates, primeNumber, likeToVisit, heritage, diy
+// userName, likesTravel, likesDogs, scubaDive, hasPets, rickMorty, procrastinates, primeNumber, likeToVisit, heritage, diy
+
+// Initial message
+alert('Hello, I\'m Brett! Happy to have you here. I would like to ask you some questions to get to know each other.');
+
+var userName = prompt('Let\'s start with your first name. I\'d like to know who I\'m talking with!');
+console.log('The user\'s name is: ' + userName);
+
+alert('Happy to meet you, ' + userName + '! Thank you for coming here. Let\'s begin.');
+
+// Game questions begin here
+var likesDogs = prompt('Do you enjoy spending time with dogs?');
+console.log('Does ' + userName + ' like dogs? : ' + likesDogs);
+
+var likesTravel = prompt('Do you like to travel around at all?');
+console.log('Does ' + userName + ' like to travel? : ' + likesTravel);
+
+var likeToVisit = prompt('Where is somewhere you would like to visit?');
+console.log('Where is somewhere ' + userName + ' would like to visit? : ' + likeToVisit);
+
 
